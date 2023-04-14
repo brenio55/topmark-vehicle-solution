4df9c32721f139f936fe4f69313bd1c38dbf3f8e
brenio55 <brenio55@outlook.com>
2023-04-14 00:01:43 -0300

    Criado HTML BÁSICO da sessão administrativa e criado header básico

--- adm_ger/index.php
+++ adm_ger/index.php
@@ -5,8 +5,55 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
-    
+    <header>
+        <div class="logo">
+            <img src="" alt="logo" class="logo">
+        </div>    
+        <nav>
+            <ul>
+                <li>WELCOME</li>
+                <li>SERVICES</li>
+                <li>Showroom</li>
+                <li>About Us</li>
+            </ul>
+        </nav>
+        <div class="conta">
+            <?php 
+                if (isset($_SESSION)){
+                    echo "
+                        sessão iniciada.
+                    ";
+                }else{
+                    echo "<span>Login</span>";
+                    echo "<span>Sign up</span>";
+                }
+            ?>
+        </div>
+    </header>
+    <nav class="admin">
+        <ul>
+            <li>DASHBOARD</li>
+            <li>POSTS</li>
+            <li>COMMENTS</li>
+            <li>USERS</li>
+        </ul>
+    </nav>
+    <main></main>
+    <footer>
+        <div class="logo">
+                <img src="" alt="logo" class="logo">
+            </div>    
+            <nav>
+                <ul>
+                    <li>WELCOME</li>
+                    <li>SERVICES</li>
+                    <li>Showroom</li>
+                    <li>About Us</li>
+                </ul>
+            </nav>'
+    </footer>
 </body>
 </html>

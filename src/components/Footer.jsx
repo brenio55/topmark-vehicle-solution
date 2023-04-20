c22b245f0138ce04aaa4e63fdbe3b5453e3d1524
brenio55 <brenio55@outlook.com>
2023-04-19 22:59:45 -0300

    Adicionado arquivos e CSS anterior ao projeto React. Também adicionadas rotas

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+function Footer(){
+    return (
+        <>
+        <footer>
+            <div class="logo">
+                    <img src="" alt="logo" class="logo" />
+                </div>    
+                <nav>
+                    <ul>
+                        <li>WELCOME</li>
+                        <li>SERVICES</li>
+                        <li>SHOWROOM</li>
+                        <li>ABOUT US</li>
+                    </ul>
+                </nav>
+        </footer>
+        </>
+    )
+}
+
+export default Footer

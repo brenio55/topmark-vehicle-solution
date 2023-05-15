2c8a2db4390b33316197b8aed43e513d9251e393
brenio55 <brenio55@outlook.com>
2023-05-15 17:20:27 -0300

    Adicionado carrosel e estilos do carrosel. Faltando adicionar nomes e partes de trás

--- src/pages/main/sections/CurrentStock.jsx
+++ src/pages/main/sections/CurrentStock.jsx
@@ -1,6 +1,57 @@
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import { AutoScroll } from '@splidejs/splide-extension-auto-scroll';
+// new Splide( '.splide' ).mount( { AutoScroll } );
+
+import '@splidejs/splide/css';
+import Car from './CurrentStock/Car';
+
+
 function CurrentStock(){
     return (
         <div>
+           <div className="currentStockDIV">
+                <h2>CURRENT STOCK</h2>
+
+                <Splide aria-label="My Favorite Images"
+                    options={{
+                        type   : 'loop',
+                        drag   : 'free',
+                        focus  : 'center',
+                        perPage: 4,
+                        autoScroll: {
+                          speed: 0.4,
+                        },
+                        gap: '8px'
+                    }}
+                    extensions={{ AutoScroll }}
+                >
+                    <SplideSlide>
+                        <Car carSrc="src\img\cars\car1.png"
+                             carName="BMW M4"
+                             carPrice="0,000"
+                        />
+                    </SplideSlide>
+                    <SplideSlide>
+                        <Car carSrc="src\img\cars\car2.png"
+                             carName="BMW 7 Series"
+                             carPrice="0,000"
+                        />
+                    </SplideSlide>
+                    <SplideSlide>
+                        <Car carSrc="src\img\cars\car3.png"
+                             carName="BMW Z4 "
+                             carPrice="0,000"
+                        />
+                    </SplideSlide>
+                    <SplideSlide>
+                        <Car carSrc="src\img\cars\car4.png" 
+                             carName="BMW M3"
+                             carPrice="0,000"
+                        />
+                    </SplideSlide>
+                </Splide>
+           </div>
+
            
         </div>
     )
---
+++ src/pages/main/sections/CurrentStock/Car.jsx
@@ -0,0 +1,17 @@
+function Car({carSrc, carName, carPrice}){
+    return (
+        <div>
+           <div className="carSquare">
+                <div className="topSide">
+                    <img className="carImg" src={carSrc} alt={carName}/>    
+                </div>
+                <div className="botSide">
+                    <h3>{carName}</h3>
+                    <h4>£{carPrice}</h4>
+                </div>
+           </div>
+        </div>
+    )
+}
+
+export default Car

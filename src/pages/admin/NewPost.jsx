833a1bc41e17fff2d7028068290c4115c30f18db
brenio55 <brenio55@outlook.com>
2023-06-30 01:13:30 -0300

    Adicionada conexão com o banco de dados para que se possa inserir os dados do carro. Problema ao enviar imagem.

--- src/pages/admin/NewPost.jsx
+++ src/pages/admin/NewPost.jsx
@@ -1,7 +1,42 @@
+import { useState } from "react"
 import Footer from "../../components/Footer"
 import Header from "../../components/Header"
+import axios from 'axios';
 
 function NewPost(){
+    const [selectedFile, setSelectedFile] = useState(null);
+    const uploadApiSrc = import.meta.env.VITE_UPLOADAPI
+
+    function sendCarApi(event){
+        event.preventDefault();
+        let carsName = event.target[0].value
+        let carsPrice = event.target[1].value
+        
+
+        if (selectedFile){
+            
+            const carsImage = new FormData();
+            carsImage.append('image', selectedFile);
+
+            axios.defaults.headers.post['Content-Type'] = 'application/json';
+            axios.post(uploadApiSrc, 
+                {user: 'admin', password: 'admin', 
+                carsName: carsName, carsPrice: carsPrice,
+                carsImage: carsImage})
+            .then(response => {
+                console.log('Upload bem-sucedido!', response.data)
+            })
+            .catch(error => {
+                console.log('Upload mal-sucedido!', error)
+            });
+        }
+    }
+
+    const fileSet = function(event){
+        setSelectedFile(event.target.files[0]);
+        
+    }
+
     return (
         <>
         <Header onAdmin={true} />
@@ -22,7 +57,7 @@ function NewPost(){
                         <h2>Information</h2>
                     </article>
                     <article className="infoAddPost">
-                        <form action="">
+                        <form onSubmit={sendCarApi}>
                             <label htmlFor="carsName">Car's Name</label><br/>
                             <input type="text" name="carsName" id="" placeholder=""/><br/>
 
@@ -30,9 +65,9 @@ function NewPost(){
                             <input type="number" name="carsPrice" id="" placeholder=""/><br/>
 
                             <label htmlFor="carsImage">Car's Image</label><br/>
-                            <input type="file" name="carsImage" id="" placeholder=""/><br/>
+                            <input type="file" name="carsImage" id="" placeholder="" onChange={fileSet} /><br/>
 
-                            
+                            <button type="submit">Enviar</button>
                         </form>
                     </article>
                     {/* <article className="dashboardAction">
